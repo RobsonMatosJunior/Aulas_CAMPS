a90ae7c351440cb47c01c0cc1e9b7110c4803158
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-10-20 11:16:55 -0300

    atividades aula 03

---
+++ Aula03/01/Program.cs
@@ -0,0 +1,14 @@
+﻿// ## Cálculo do Perímetro de um Círculo (Usando Constantes) ##
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a velocidade em km/h: ");
+        float velocidadeKmh = float.Parse(Console.ReadLine());
+
+        float velocidadeMs = velocidadeKmh / 3.6f;
+        Console.WriteLine($"A velocidade em m/s é: {velocidadeMs}");
+    }
+}
---
+++ Aula03/02/Program.cs
@@ -0,0 +1,16 @@
+﻿// ## Cálculo do Perímetro de um Círculo (Usando Constantes) ##
+
+using System;
+class Program
+{
+    const double PI = 3.14159;
+
+    static void Main()
+    {
+        Console.Write("Digite o raio do círculo: ");
+        float raio = float.Parse(Console.ReadLine());
+
+        double perimetro = 2 * PI * raio;
+        Console.WriteLine($"O perímetro do círculo é: {perimetro}");
+    }
+}
