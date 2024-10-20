6ffb95f9230a0d2183b1673ecc0848f8ac99ec93
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-10-20 11:28:25 -0300

    Atividades da aula 04

---
+++ Aula04/02/Program.cs
@@ -0,0 +1,21 @@
+﻿// ## Aprovação de Aluno: Determine se um aluno foi aprovado ou reprovado com base em uma nota.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a nota do aluno: ");
+        float nota = float.Parse(Console.ReadLine());
+
+        if (nota >= 6)
+        {
+            Console.WriteLine("Aluno aprovado.");
+        }
+        else
+        {
+            Console.WriteLine("Aluno reprovado.");
+        }
+    }
+}
---
+++ Aula04/04/Program.cs
@@ -0,0 +1,17 @@
+﻿// ## Tabuada: Escreva um programa que imprima a tabuada de um número digitado pelo usuário.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número para ver a tabuada: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+    }
+}
