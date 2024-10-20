230e3adea6c71aaae70b8fcdfb3c1e5668bfbfa1
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-10-20 11:12:59 -0300

    Atividades aula 02

---
+++ Aula02/02/Program.cs
@@ -0,0 +1,13 @@
+﻿// ## Algoritmo para Converter Temperatura de Celsius para Fahrenheit ##
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a temperatura em Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+        double fahrenheit = (9.0 / 5.0) * celsius + 32;
+        Console.WriteLine($"{celsius} graus Celsius é igual a {fahrenheit} graus Fahrenheit.");
+    }
+}
