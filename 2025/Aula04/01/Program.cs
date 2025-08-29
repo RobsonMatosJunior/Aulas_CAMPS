f24e7dbda7cf034a65a723f4f7d07686c600f7a6
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2025-08-28 23:52:06 -0300

    AUla 04

---
+++ 2025/Aula04/01/Program.cs
@@ -0,0 +1,12 @@
+﻿int contador = 3;
+
+Console.WriteLine("Iniciando contagem...");
+while (contador > 0)
+
+{
+Console.WriteLine(contador);
+contador = contador - 1; // Diminui 1 do contador a cada volta
+}
+
+Console.WriteLine("Lançar!");
+// --------------------------------------------------------------------------------
---
+++ 2025/Aula04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Começamos com um contador no valor 1.
+int contador = 1;
+// O loop vai continuar ENQUANTO o 'contador' for menor ou igual a 5.
+while (contador <= 5)
+{
+    // Mostra o valor atual do contador na tela.
+    Console.WriteLine(contador);
+
+    // ESSENCIAL: Aumentamos o contador em 1.
+    // Sem esta linha, o contador seria sempre 1 e o loop nunca terminaria!
+    contador = contador + 1;
+}
+
+--------------------------------------------------------------------------------
+
+
+for (int i = 1; i <= 5; i++)
+int i = 1; //Comece a contar a partir de 1.
+i <= 5; //Continue repetindo enquanto o contador for menor ou igual a 5.
+i++ //No final de cada repetição, some 1 ao contador.
+
+
+--------------------------------------------------------------------------------
+
